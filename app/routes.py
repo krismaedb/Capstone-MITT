bd6665b61b2e8f76db4dc4eb86ff25f245836d9d
krismaedb <maebagsican7@gmail.com>
2025-12-09 13:48:37 -0600

    text inadd

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 from datetime import datetime, date
 from . import db
 from .models import User, Patient, Appointment
+from sqlalchemy import text
 
 main_bp = Blueprint('main', __name__)
 
